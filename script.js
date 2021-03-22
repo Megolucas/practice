63a97b7ff80fb47d1c9e37e03c817e2ec8334531
Alex <sokolovleha94@gmail.com>
2021-03-22 22:46:25 +0200

    practice1

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('один из последних просмотреных фильмов', ''),
+    b = prompt('На сколько оцените его', ''),
+    c = prompt('один из последних просмотреных фильмов', ''),
+    d = prompt('На сколько оцените его', '');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
